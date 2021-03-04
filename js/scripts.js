7f02d4e2e6de20593856d0a67f3f7163bf11863c
begomart <bmgoitia@gmail.com>
2021-03-04 21:36:47 +0100

    first push

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+window.onload = function() {
+ 
+ 		$(".tlDateY").parent().css( "margin-bottom", "30px" );
+ 		$(".tlDateM").parent().css( "margin-bottom", "50px" );
+    $(".tlDateM").parent().css( "background-color", "#000" );
+
+
+    /* Slider */
+
+    var mySwiper = new Swiper('.swiper-container', {
+			  // Optional parameters
+			  direction: 'horizontal',
+			  loop: false,
+
+			  pagination: {
+			    el: '.swiper-pagination',
+			  },
+
+			  navigation: {
+			    nextEl: '.swiper-button-next',
+			    prevEl: '.swiper-button-prev',
+			  },
+
+
+    })
+
+ 
+};
+
+/* Files */
+
+$(".testListHead").click(function(){
+  $(".testListHead span").toggleClass("arrowUp");
+});
+
