1464dad05ceced654691ddfe2edac010f159189b
begomart <bmgoitia@gmail.com>
2021-03-06 10:30:19 +0100

    slider: remove swiper, replace it with Bootstra carousel

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,7 @@ window.onload = function() {
     })
 
 
-    /* Slider */
-
-    var mySwiper = new Swiper('.swiper-container', {
-			  // Optional parameters
-			  direction: 'horizontal',
-			  loop: false,
-
-			  pagination: {
-			    el: '.swiper-pagination',
-			  },
-
-			  navigation: {
-			    nextEl: '.swiper-button-next',
-			    prevEl: '.swiper-button-prev',
-			  },
-
-
-    })
+    
 
  
 };
